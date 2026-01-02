9e20ba804ee48eade989e30f0f2877171c95cb0d
kajt <kajt.kajt.kajt.kajt.kajt@gmail.com>
2026-01-02 19:16:35 +0200

    task 3 finished

--- task03.py
+++ task03.py
@@ -6,15 +6,18 @@ from task03_alg01 import kmp_search
 from task03_alg02 import boyer_moore_search
 from task03_alg03 import rabin_karp_search
 
+# function to draw graph
 def draw_graph(x, y, x_label, y_label, title, filename):
+    """
+    Draw graph and save it to specified file
+    """
     fig, ax = plt.subplots()
-    for algo in algorithm_colors:
+    for algo, color in algorithm_colors.items():
         ax.plot(x, y[algo], 
-                f'{algorithm_colors[algo]}o', 
+                f'{color}o', 
                 label=algo, 
                 markersize=5, 
                 alpha=0.67)
-    
     ax.set_xlabel(x_label)
     ax.set_ylabel(y_label)
     ax.set_title(title)
@@ -23,17 +26,15 @@ def draw_graph(x, y, x_label, y_label, title, filename):
     plt.savefig(filename)
 
 # Let's read input files
-article1 = ""
-article2 = ""
-
-with open("./article1.txt","r") as f:
+with open("./article1.txt", "r", encoding="utf-8") as f:
     article1 = "".join(f.readlines())
 
-with open("./article2.txt","r") as f:
+with open("./article2.txt", "r", encoding="utf-8") as f:
     article2 = "".join(f.readlines())
 
 text_length = min(len(article1), len(article2))
 
+# Let's prepare input data
 REPEATS_PER_TEST = 10
 NUMBER_OF_TESTS = 10
 pattern_sizes = [10]*NUMBER_OF_TESTS +\
@@ -67,19 +68,26 @@ for i in range(len(pattern_sizes)):
     random_string_1.append("".join([random.choice(string.ascii_letters) for _ in range(pattern_sizes[i])]))
     random_string_2.append("".join([random.choice("ab") for _ in range(pattern_sizes[i])]))
 
+# Testing
 for algo in algorithms:
-    for i in range(len(pattern_sizes)):
-        n = pattern_sizes[i]
+    for i, n in enumerate(pattern_sizes):
         start = start_positions[i]
         pattern1 = article1[start:start+n]
         pattern2 = article2[start:start+n]
-        algo_result_1[algo].append(timeit.timeit('algorithms[algo](article1,pattern1)', number=REPEATS_PER_TEST, globals=globals()))
-        algo_result_2[algo].append(timeit.timeit('algorithms[algo](article2,pattern2)', number=REPEATS_PER_TEST, globals=globals()))
-        algo_result_1_random[algo].append(timeit.timeit('algorithms[algo](article1,random_string_1[i])', number=REPEATS_PER_TEST, globals=globals()))
-        algo_result_2_random[algo].append(timeit.timeit('algorithms[algo](article2,random_string_1[i])', number=REPEATS_PER_TEST, globals=globals()))
-        algo_result_1_ab[algo].append(timeit.timeit('algorithms[algo](article1,random_string_2[i])', number=REPEATS_PER_TEST, globals=globals()))
-        algo_result_2_ab[algo].append(timeit.timeit('algorithms[algo](article2,random_string_2[i])', number=REPEATS_PER_TEST, globals=globals()))
+        algo_result_1[algo].append(timeit.timeit('algorithms[algo](article1,pattern1)', 
+                                                 number=REPEATS_PER_TEST, globals=globals()))
+        algo_result_2[algo].append(timeit.timeit('algorithms[algo](article2,pattern2)', 
+                                                 number=REPEATS_PER_TEST, globals=globals()))
+        algo_result_1_random[algo].append(timeit.timeit('algorithms[algo](article1,random_string_1[i])',
+                                                        number=REPEATS_PER_TEST, globals=globals()))
+        algo_result_2_random[algo].append(timeit.timeit('algorithms[algo](article2,random_string_1[i])',
+                                                        number=REPEATS_PER_TEST, globals=globals()))
+        algo_result_1_ab[algo].append(timeit.timeit('algorithms[algo](article1,random_string_2[i])',
+                                                    number=REPEATS_PER_TEST, globals=globals()))
+        algo_result_2_ab[algo].append(timeit.timeit('algorithms[algo](article2,random_string_2[i])',
+                                                    number=REPEATS_PER_TEST, globals=globals()))
 
+# Drawing graphs
 draw_graph(
     pattern_sizes,
     algo_result_1,
